50488bacfb84c09429fe11b174a9f6da00f71742
Colin Taaffe <85810714+BurnerTV@users.noreply.github.com>
2024-07-31 15:04:48 -0500

    finished all handlebars and added index.js controller

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sequelize = require('./config/config');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+const helpers = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
