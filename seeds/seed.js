973ba1a1e289b0fe868f61fb7b21cf37f1990295
Colin Taaffe <85810714+BurnerTV@users.noreply.github.com>
2024-07-31 17:03:25 -0500

    edited seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,23 +1,34 @@
 const sequelize = require('../config/config');
 const { User, Post, Comment } = require('../models');
 
-const userData = [
-    {
-        username: 'johndoe',
-        email: 'johndoe@example.com',
-        password: 'password123'
-    },
-];
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
-    await sequelize.sync({ force: true });
+  await sequelize.sync({ force: true });
 
-    const users = await User.bulkCreate(userData, {
-        individualHooks: true,
-        returning: true,
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const post of postData) {
+    await Post.create({
+      ...post,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
     });
+  }
 
-    process.exit(0);
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+      post_id: posts[Math.floor(Math.random() * posts.length)].id,
+    });
+  }
+
+  process.exit(0);
 };
 
 seedDatabase();
