46bc780b2b8adacf2bb8bb24022d5d4cf210c0f2
Colin Taaffe <85810714+BurnerTV@users.noreply.github.com>
2024-07-31 17:01:55 -0500

    edited config.js

--- config/config.js
+++ config/config.js
@@ -3,14 +3,14 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: 'localhost',
-        dialect: 'postgres',
-        port: 5432,
-    }
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+    port: process.env.DB_PORT || 5432,
+  }
 );
 
 module.exports = sequelize;
