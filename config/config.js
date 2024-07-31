963d0f7363343c5e744cc73c3701511a9df2c2bf
Colin Taaffe <85810714+BurnerTV@users.noreply.github.com>
2024-07-31 17:35:59 -0500

    render not working

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST || '127.0.0.1',
+    host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres',
     port: process.env.DB_PORT || 5432,
   }
